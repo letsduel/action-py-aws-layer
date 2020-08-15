29bca6513610679296cb02b5332e628272cb470d
Ope Usman <mail.opeusman@gmail.com>
2020-08-14 23:58:42 -0400

    updated

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ zipping_code(){
 }
 
 publishing_as_layer(){
-	echo "Publishing as a layer..."
-	echo "${lambda_layer}"
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://code.zip --region us-east-1)
+	echo "Publishing as a ${lambda_layer} layer..."
+
+	local result=$(aws lambda publish-layer-version --layer-name "${lambda_layer}" --zip-file fileb://code.zip --region us-east-1)
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
 	rm -rf python
 	rm code.zip
@@ -19,7 +19,7 @@ publishing_as_layer(){
 
 update_function_layers(){
 	echo "Using the layer in the functions..."
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}" --region "us-east-1"
+	aws lambda update-function-configuration --function-name "${lambda_functions}" --layers "${lambda_layer}:${LAYER_VERSION}" --region "us-east-1"
 }
 
 deploy_aws_layer(){
